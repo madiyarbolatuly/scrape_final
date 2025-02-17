18d15ea6d7221b72a93aa94dde6c9efde376a17a
Madiyar <57250791+madiyarbolatuly@users.noreply.github.com>
2025-02-17 10:06:22 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -21,11 +21,10 @@ for folder in [app.config['UPLOAD_FOLDER'], app.config['OUTPUT_FOLDER']]:
     
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
+chrome_options = Options()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-
-chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
