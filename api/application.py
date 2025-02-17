37bf646846ab1d6edc8607f2e40b1135bd953e8f
Madiyar <57250791+madiyarbolatuly@users.noreply.github.com>
2025-02-17 16:29:17 +0500

    Update application.py

--- api/application.py
+++ api/application.py
@@ -1,6 +1,7 @@
-from api.application import create_app
+from api import create_app, socketio
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    # Use socketio.run() instead of app.run() when using Flask-SocketIO.
+    socketio.run(app, host="0.0.0.0", port=5000)
