770ae77d821b0d06f022758a95f147130459fabb
Madiyar <57250791+madiyarbolatuly@users.noreply.github.com>
2025-02-18 07:53:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = create_app()
 
 if __name__ == "__main__":
     # For development with Flask-SocketIO:
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=8080, debug=True)
